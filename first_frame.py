5d9ad0d40c1bf4a88c8e2d2556f5cd2eada98e92
Andre Pilco <andrepilcoch@gmail.com>
2023-09-19 21:42:54 -0500

    mejora filtros

--- first_frame.py
+++ first_frame.py
@@ -1,10 +1,13 @@
 import cv2
 
-def get_first_frame(video_path):
+def get_frame(video_path, frame_number):
     # Abre el video
     cap = cv2.VideoCapture(video_path)
-    # Obtiene el primer frame
-    ret, frame = cap.read()
+    # Avanza hasta el frame especificado
+    for i in range(frame_number):
+        ret, frame = cap.read()
+        if not ret:
+            raise ValueError(f"El vídeo no tiene {frame_number} frames.")
     # Cierra el video
     cap.release()
     # Devuelve el frame
@@ -14,8 +17,8 @@ def get_first_frame(video_path):
 # Ejemplo de uso
 video_path = "prueba.mp4"
 
-# Obtiene el primer frame
-frame = get_first_frame(video_path)
+# Obtiene el frame 1500
+frame = get_frame(video_path, 1500)
 
 # Muestra el frame
 cv2.imshow("table.jpg", frame)
--- main.py
+++ main.py
@@ -17,19 +17,20 @@ table = [(0,1), (2,3,4,5), (6,7,8),(9,10,11,13)]
 table_len = len(table)
 table_occ = ['Free'] * table_len
 
-px = 2500
+px = 3200
 while True:
     check, img = video.read()
     imgBN = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgTH = cv2.adaptiveThreshold(imgBN, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
-    imgMedian = cv2.medianBlur(imgTH, 5)
-    kernel = np.ones((5,5), np.int8)
+    imgTH = cv2.adaptiveThreshold(imgBN, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 9)
+    imgMedian = cv2.medianBlur(imgTH, 1)
+    kernel = np.ones((3,3), np.int8)
     imgDil = cv2.dilate(imgMedian, kernel)
     for x, y, w, h in seat:
         rec = (x, y, w, h)
         espacio = imgDil[y:y+h, x:x+w]
         count = cv2.countNonZero(espacio)
-        cv2.putText(img, str(seat.index(rec))+ ', ' + str(occ[seat.index(rec)]) + ', ' + str(count), (x,y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        # cv2.putText(img, str(seat.index(rec))+ ', ' + str(occ[seat.index(rec)]) + ', ' + str(count), (x,y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        cv2.putText(img, 'Silla ' + str(seat.index(rec)+1), (x,y+h-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
         cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
         occ[seat.index(rec)] = 1
         if count < px:
@@ -44,14 +45,15 @@ while True:
                 zeros += 1
         if zeros == len(table[tables.index(rec)]): table_occ[tables.index(rec)] = 'Free'
         else: table_occ[tables.index(rec)] = 'Full'
-        cv2.putText(img, 'Mesa '+ str(tables.index(rec)) + ': ' + str(table_occ[tables.index(rec)]), (200 * tables.index(rec), 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1)
-        cv2.putText(img, str(tables.index(rec))+ ', ' + str(occ[tables.index(rec)]) + ', ' + str(count), (x,y+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
-        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
-        if table_occ[tables.index(rec)] == 'Free':
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+        cv2.putText(img, 'Mesa '+ str(tables.index(rec)+1) + ': ' + str(table_occ[tables.index(rec)]), (200 * tables.index(rec), 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,0), 1)
+        # cv2.putText(img, str(tables.index(rec))+ ', ' + str(occ[tables.index(rec)]) + ', ' + str(count), (x,y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        cv2.putText(img, 'Mesa ' + str(tables.index(rec)+1), (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
+        # cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+        # if table_occ[tables.index(rec)] == 'Free':
+            # cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
 
     cv2.imshow('video', img)
     # cv2.imshow('video TH', imgTH)
     # cv2.imshow('video Median', imgMedian)
-    cv2.imshow('video Dilatada', imgDil)
+    # cv2.imshow('video Dilatada', imgDil)
     cv2.waitKey(10)
