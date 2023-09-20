397524eab9a067d933a752a7538f29928d74c09a
JohnSovero <u202115065@upc.edu.pe>
2023-09-19 22:52:49 -0500

    .

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ occ = [1] * len(seat)
 table = [(0,1), (2,3,4,5), (6,7,8),(9,10,11,13)]
 table_len = len(table)
 table_occ = ['Free'] * table_len
-print(seat)
+
 px = 3600
 while True:
     check, img = video.read()
